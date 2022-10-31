d19b11e16a2e54903bb8dd5d5cfce87d66e41594
Christian Afriem <cafriem@lab1r4s1.42abudhabi.ae>
2022-10-31 16:09:42 +0400

    update 1

--- sort_command_rr.c
+++ sort_command_rr.c
@@ -6,7 +6,7 @@
 /*   By: cafriem <cafriem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 15:47:54 by cafriem           #+#    #+#             */
-/*   Updated: 2022/10/31 15:47:58 by cafriem          ###   ########.fr       */
+/*   Updated: 2022/10/31 16:08:18 by cafriem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	rra(t_stack *stack)
 	int	c;
 
 	c = ft_endstack(stack->astack);
-	num = stack->astack[0];
+	num = stack->astack[c];
 	while (c != 1)
 	{
-		stack->astack[c] = stack->astack[c];
+		stack->astack[c] = stack->astack[c - 1];
 		c--;
 	}
-	stack->astack[c - 1] = num;
-	stack->astack[c] = NULL;
+	stack->astack[0] = num;
+	stack->astack[ft_endstack(stack->astack)] = 0;
 }
