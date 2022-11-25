3feb35d6903523f3eae35e5c44ce1f94d1da4066
Christian Afriem <cafriem@lab1r4s1.42abudhabi.ae>
2022-11-25 17:31:45 +0400

    not working 100

--- push_swaplong.c
+++ push_swaplong.c
@@ -6,7 +6,7 @@
 /*   By: cafriem <cafriem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 17:35:55 by cafriem           #+#    #+#             */
-/*   Updated: 2022/11/24 14:30:06 by cafriem          ###   ########.fr       */
+/*   Updated: 2022/11/25 17:31:01 by cafriem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,25 @@
 
 void	sort100(t_stack *stack)
 {
+	int	c;
+	int	c1;
+	int	chunk;
+
+	c = 0;
+	c1 = 0;
 	stack->chunk_size = abs(stack->size / 5);
 	stack->mid = abs(stack->size / 2);
 	minandmin2(stack);
 	close_checker(stack);
+	chunk = stack->chunk_size;
+	while (c < stack->size)
+	{
+		while (c < chunk)
+		{
+			c1 = 0;
+			if ()
+			rra(stack);
+			c++;
+		}
+	}
 }
